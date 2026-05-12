dfc868a8a48269bda551f013b98a6f2b4e4acc8c
Tinh516725 <tinhnq212@gmail.com>
2026-05-12 14:16:33 +0700

    qưcce

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/MyCv",
-  plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: true
-  }
+  plugins: [react()]
 });
